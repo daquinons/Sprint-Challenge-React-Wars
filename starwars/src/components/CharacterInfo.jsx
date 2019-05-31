b63beb99699d055f80bedcd744dca7aa1793ada8
David Quiñones <daquinons@users.noreply.github.com>
2019-05-31 09:50:51 +0200

    Add basic style

--- starwars/src/components/CharacterInfo.jsx
+++ starwars/src/components/CharacterInfo.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './StarWars.css';
 
 const CharacterInfo = ({ character }) => {
   console.log(character);
   return(
-    <div>
+    <div className="character-card">
       <p>{character.name}</p>
       <p>{character.height} cm</p>
       <p>{character.mass} kg</p>
--- starwars/src/components/CharacterList.jsx
+++ starwars/src/components/CharacterList.jsx
@@ -3,13 +3,15 @@ import CharacterInfo from './CharacterInfo';
 
 const CharacterList = ({ characterList }) => {
   return (
-    characterList.map((character, index) => {
-      return (
-        <div key={index}>
-          <CharacterInfo character={character} />
-        </div>
-      )
-    })
+    <div className="character-list">
+      {characterList.map((character, index) => {
+        return (
+          <div key={index}>
+            <CharacterInfo character={character} />
+          </div>
+        )
+      })}
+    </div>
   )
 }
 
